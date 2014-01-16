6aecd66eefabd8e56ce57af2384761e381065e0e
Scott Holden <sholden@truecar.com>
2014-01-15 22:04:26 -0800

    added Comment.training_data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :link, primary_key: :name, foreign_key: :link_id
   belongs_to :parent, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
   has_many :replies, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
+
+  def self.training_data
+    Comment.group(:link_id, :author).count.map{|(link, author), count| [link, author, count]}
+  end
 end
