f2e3c5eca654b0c9771abbfd88ac6467d16fa4e7
Scott Holden <sholden@truecar.com>
2014-01-15 21:59:15 -0800

    added model relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   self.primary_key = :id
+
+  belongs_to :link, primary_key: :name, foreign_key: :link_id
+  belongs_to :parent, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,7 @@ class Link < ActiveRecord::Base
   self.primary_key = 'id'
 
   scope :top, -> {order('score desc')}
+
+  has_many :comments, primary_key: :name, foreign_key: :link_id
+  has_many :root_comments, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
 end
