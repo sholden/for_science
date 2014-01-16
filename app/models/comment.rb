ea263b37e9d02baa3a9fe665cb930cb9344ac130
Scott Holden <sholden@truecar.com>
2014-01-15 22:00:14 -0800

    comments have replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
 
   belongs_to :link, primary_key: :name, foreign_key: :link_id
   belongs_to :parent, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
+  has_many :replies, class_name: 'Comment', primary_key: :name, foreign_key: :parent_id
 end
